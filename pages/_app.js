92701d6793fad20dd1fa6d7eeee610636e1fd2ac
seonudoiba <abiodunigbehinadun17@gmail.com>
2022-08-27 03:42:46 +0100

    edit font commit

--- pages/_app.js
+++ pages/_app.js
@@ -32,12 +32,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#ffffff" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&family=Rubik+Dirt&display=swap"
-          rel="stylesheet"
-        />
+        
       </Head>
       <Layout>
         <Component {...pageProps} />
