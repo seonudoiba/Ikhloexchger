e7da663a4662dbba0d86e184a995aad36b6d9ba8
seonudoiba <abiodunigbehinadun17@gmail.com>
2022-08-31 23:47:27 +0100

    contact page 1

--- pages/Contact.js
+++ pages/Contact.js
@@ -18,13 +18,13 @@ const Contact = () => {
                   <form className="main_form">
                      <div className="row">
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                           <input className="form-control" placeholder="Name" type="text" name="Name">
+                           <input className="form-control" placeholder="Name" type="text" name="Name"/>
                         </div>
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                           <input className="form-control" placeholder="Email" type="text" name="Email">
+                           <input className="form-control" placeholder="Email" type="text" name="Email"/>
                         </div>
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                           <input className="form-control" placeholder="Phone" type="text" name="Phone">
+                           <input className="form-control" placeholder="Phone" type="text" name="Phone"/>
                         </div>
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                            <textarea className="textarea" placeholder="Message" type="text" name="Message"></textarea>
